337a2c2ee803d5aa58dcc69f8ff30ad9e254934a
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-14 21:41:50 +1100

    Delete api/trpc directory

--- api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/trpc/routers'; 
-import { createContext } from '../../../server/trpc/_core/context';
-
-export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-});
