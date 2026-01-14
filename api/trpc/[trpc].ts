ba8a9223ea0cf65ff6f790fb00793d9025e6e5f4
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-14 22:15:34 +1100

    Update [trpc].ts

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,17 +1,13 @@
-import path from 'path';
 import { createNextApiHandler } from '@trpc/server/adapters/next';
-
-// In "type": "module", we use import instead of require
-// We MUST add the .js extension for local files to be found on Vercel
-import { appRouter } from '../../../server/trpc/routers.js';
-import { createContext } from '../../../server/server_core/context.js';
+import { appRouter } from '../../server/trpc/routers.js';
+import { createContext } from '../../server/server_core/context.js';
 
 export default createNextApiHandler({
   router: appRouter,
   createContext,
   onError({ error }) {
     if (error.code === 'INTERNAL_SERVER_ERROR') {
-      console.error('TRPC Error:', error);
+      console.error('TRPC Server Error:', error);
     }
   },
 });
