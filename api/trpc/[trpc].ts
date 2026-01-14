0be6796b02b95207fcf20674da40feb1d6a40d2f
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-14 21:23:56 +1100

    Update [trpc].ts

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/trpc/routers'; // Adjust if your routers are in a different spot
-import { createContext } from '../../../server/trpc/_core/context'; // We found this in your list!
+import { appRouter } from '../../../server/trpc/routers'; 
+import { createContext } from '../../../server/trpc/_core/context';
 
 export default createNextApiHandler({
   router: appRouter,
