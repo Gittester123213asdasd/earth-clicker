148831ad250d9e195b411321838b3575bba0495a
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-14 21:46:45 +1100

    Update [trpc].ts

--- api/trpc/[trpc].ts
+++ api/trpc/[trpc].ts
@@ -1,1 +1,14 @@
+const { createNextApiHandler } = require('@trpc/server/adapters/next');
+const { appRouter } = require('../../../server/trpc/routers');
+const { createContext } = require('../../../server/server_core/context');
 
+module.exports = createNextApiHandler({
+  router: appRouter,
+  createContext,
+  // This helps you see the REAL error in your Vercel logs
+  onError({ error }) {
+    if (error.code === 'INTERNAL_SERVER_ERROR') {
+      console.error('Something went wrong:', error);
+    }
+  },
+});
