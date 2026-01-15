bee339277bbb1d79e72a4cf4fc7efcebc17319b1
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-16 10:48:20 +1100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink, TRPCClientError } from "@trpc/client";
 import { createRoot } from "react-dom/client";
 import superjson from "superjson";
-import App from "./App";
+// Change this line to point to Home instead of App
+import Home from "./Home"; 
 import { getLoginUrl } from "./const";
 import "./index.css";
 
@@ -55,7 +56,8 @@ const trpcClient = trpc.createClient({
 createRoot(document.getElementById("root")!).render(
   <trpc.Provider client={trpcClient} queryClient={queryClient}>
     <QueryClientProvider client={queryClient}>
-      <App />
+      {/* Render Home directly here */}
+      <Home /> 
     </QueryClientProvider>
   </trpc.Provider>
 );
