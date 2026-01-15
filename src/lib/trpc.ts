fdabceeba182d9e894b893b9280529a32260fde1
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-16 10:49:07 +1100

    Update trpc.ts

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,5 +1,6 @@
 import { createTRPCReact } from '@trpc/react-query';
-// This import path must point to where your AppRouter type is defined in the server folder
-import type { AppRouter } from '../../../server/_core/trpc'; 
+// We use an absolute-style path relative to the root if possible, 
+// or ensure we go back enough levels (../../../)
+import type { AppRouter } from '../../server/_core/trpc'; 
 
 export const trpc = createTRPCReact<AppRouter>();
