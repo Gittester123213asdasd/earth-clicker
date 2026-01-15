ebfe2eb56ed0625d86c78c25c095750c02ab15d9
Gittester123213asdasd <Gittester1@outlook.com>
2026-01-16 10:50:06 +1100

    Update trpc.ts

--- src/lib/trpc.ts
+++ src/lib/trpc.ts
@@ -1,6 +1,5 @@
 import { createTRPCReact } from '@trpc/react-query';
-// We use an absolute-style path relative to the root if possible, 
-// or ensure we go back enough levels (../../../)
+// Up one to /lib, up two to /src, then into /server
 import type { AppRouter } from '../../server/_core/trpc'; 
 
 export const trpc = createTRPCReact<AppRouter>();
